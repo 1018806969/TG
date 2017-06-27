cc05aedbf04bac8a6ee07d111148299b0c2560e0
merlin <xxtang>
2017-06-27 11:19:59 +0800

    init resp

---
+++ TG/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TG
+//
+//  Created by Merlin on 2017/6/27.
+//  Copyright © 2017年 lq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
