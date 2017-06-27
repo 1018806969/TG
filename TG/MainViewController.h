1d4435215cdf8f11a5d2393c52193cf2756cd14d
merlin <xxtang>
2017-06-27 11:26:37 +0800

    lzhou_dev init something & create mainvc

---
+++ TG/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  TG
+//
+//  Created by Merlin on 2017/6/27.
+//  Copyright © 2017年 lq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
--- TG/ViewController.m
+++ TG/ViewController.m
@@ -16,10 +16,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self initSomething];
 }
 
-
+- (void)initSomething
+{
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
